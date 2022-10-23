241867b87d8c08241d8726a2c491b7305ea3c037
RjDevOnrun <john.roby76@gmail.com>
2022-10-23 19:59:30 +0900

    Initial

---
+++ PRESENTATION/WEB/SmartData.Pid.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartData.Pid.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
